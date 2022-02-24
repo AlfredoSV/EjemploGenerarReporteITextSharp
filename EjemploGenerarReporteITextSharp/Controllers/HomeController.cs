6bf26d557e425e16cf704103ab5e6198bae9bd22
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-02-24 17:59:53 -0600

    Se agrega bibliteca ItexSharp y método de acción para generar reporte

--- EjemploGenerarReporteITextSharp/Controllers/HomeController.cs
+++ EjemploGenerarReporteITextSharp/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
 
 namespace EjemploGenerarReporteITextSharp.Controllers
 {
@@ -18,6 +22,32 @@ namespace EjemploGenerarReporteITextSharp.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        public ActionResult GenerarReporte()
+        {
+            MemoryStream fl;
+
+            using (Document pdfReporte = new Document())
+            {
+                fl = new MemoryStream();
+                PdfWriter pdfWriter = PdfWriter.GetInstance(pdfReporte, fl);
+                pdfReporte.Open();
+
+                var plantilla = new StreamReader(HttpContext.Server.MapPath("~/plantillasHtmlReportes/PlantillaReporteVentas.html"));
+
+                var con = plantilla.ReadToEnd();
+
+                StringReader str = new StringReader(con);
+
+                XMLWorkerHelper.GetInstance().ParseXHtml(pdfWriter, pdfReporte, str);
+
+
+            }
+
+            return File(fl.ToArray(), "application/pdf", "file_name.pdf");
+
+
         }
 
         public ActionResult Contact()
